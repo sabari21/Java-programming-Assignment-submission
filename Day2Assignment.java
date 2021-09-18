70d0b0c365720022dd91ca5aef99bc58b5848732
sabari21 <68653146+sabari21@users.noreply.github.com>
2021-09-18 17:49:07 +0530

    Add files via upload

---
+++ Day2Assignment.java
@@ -0,0 +1,18 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Day2Assignment 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.print("enter the number : ");
+		int num1=input.nextInt();
+		System.out.print("enter the number : ");
+		int num2=input.nextInt();
+		int Sum=num1+num2;
+		System.out.println("The sum of two numbers : "+Sum);
+	}
+
+}
